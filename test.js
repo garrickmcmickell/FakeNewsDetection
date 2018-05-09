f11e90d446749d8a14bd249e52b17e06cb319ac4
Garrick McMickell <garrickmcmickell@gmail.com>
2018-05-09 03:15:09 -0600

    Completely restructured the data sent to MongoDB from Node. Modified node to be able to send $ as key [$]. Python queries new MongoDB data, formats it, fills in the holes, and formats it again. Data is then vectorized and features are selected by SelectPercentile, using chi2 method. Data is then classified using SVM. Weights are then calculated for all three selection/classification types and plotted on a graph. Results seem correct, as highest weights are NP, NPNP, NNP, and VBZ, which should be the most used in a sentence. Chi2 works with a smaller sample size, but the reason that none of this worked previously is because the sample size was too small. F_classif might still work, but I think the sample size is going to need to be larger.

--- test.js
+++ test.js
@@ -7,7 +7,7 @@ const props = new Properties({
 })
 const pipeline = new Pipeline(props, 'English') 
 
-const doc = new CoreNLP.simple.Document('Friday’s summit meeting between the leaders of North and South Korea was a master class in diplomatic choreography, with each scene arranged for its power as political theater and broadcast live. In a perilous standoff that has resisted solutions, it was these images that offered hope, much more than the actual results from the meeting — vague pledges to work toward nuclear disarmament and a peace treaty.')
+const doc = new CoreNLP.simple.Document("There was late worry that Blankenship was gaining steam in the race's final days, but he ended up finishing behind Patrick Morrisey, the state attorney general, and Rep. Evan Jenkins, who was seen as the establishment favorite in the race. Such concern was so deep that the president himself weighed in Monday morning on Twitter to warn that Blankenship could repeat the loss of the disastrous Alabama nominee that cost the GOP a winnable seat last year. When Blankenship conceded on Tuesday night in remarks to supporters, he blamed Trump's finger on the scale for the late flameout. Blankenship, the former CEO of Massey Energy, spent a year in federal prison for violating safety laws after an explosion at one of his mines killed 29 men. He put millions of dollars of his own wealth into ads to try to rehabilitate his image, and many saw his outsider persona and way he talked of being a victim of the GOP establishment as very Trumplike. In the final days of the race, an all-out battle between Blankenship and Republican leadership erupted after Blankenship ran ads with racially charged attacks on Senate Majority Leader Mitch McConnell's Asian in-laws. Morrisey had the endorsement of Texas Sen. Ted Cruz and pitched himself as the true conservative in the race. But his opponents had criticized him as a carpetbagger after just moving to the state in 2006, and Democrats will also revive attacks on his past lobbying for pharmaceutical companies as a liability in the general election. Like many Democrats, incumbent Manchin is raising a lot of money compared with Republicans. But a little-known progressive candidate whom he outspent $2.7 million to $130,000 still managed to get 30 percent of the vote against him in the primary, which could be a worrisome sign for the centrist Manchin if the state's base isn't fully behind him.")
 //var parser = new CoreNLP.simple.annotator.ParserAnnotator({'binaryTrees': 'true'});
 //doc.addAnnotator(parser);
 var paragraph = {}
@@ -15,6 +15,7 @@ var paragraph = {}
 pipeline.annotate(doc)
   .then(doc => {
     var arr = []
+    
     for(var i = 0; i < doc.sentences().length; i++) {
       //paragraph['Sentence ' + i] = { 'sentPosWordPosStruct' : '', 'sentPosWordStruct' : '', 'sentPosStruct' : '', 'wordPosStruct' : '', 'sentPosCount' : {} , 'wordPosCount' : {} , 'punctCount' : {}, 'tree' : {} }
       paragraph['Sentence' + i] = {}
@@ -22,15 +23,17 @@ pipeline.annotate(doc)
       //var test = CoreNLP.util.Tree._buildTree(doc.sentence(i).sentimentTree())    
       //paragraph['Sentence ' + i]['tree'] = getNode(tree, tree.rootNode, i)
       //console.log(JSON.stringify(paragraph));
-      
-      format(tree.rootNode, arr)
-      console.log(arr)
+      var obj = {}
+      //format(tree.rootNode, arr)
+      format2(tree.rootNode, obj);
+      arr.push(obj);
+      //console.log(arr)
     }
 
     MongoClient.connect(url, function(err, db) {
       if (err) throw err;
       var dbo = db.db("jsAppTest");
-      dbo.collection("textData").insertOne({arr}, function(err, res) {
+      dbo.collection("textData").insertMany(arr, function(err, res) {
         if (err) throw err;
         console.log("1 document inserted");
         db.close();
@@ -43,6 +46,19 @@ pipeline.annotate(doc)
   .catch(err => {
     console.log('err', err);
   });
+
+function format2(node, obj = {}) {
+  if(node.pos() !== '$') {
+    obj[node.pos()] = node.pos() in obj ? obj[node.pos()] + 1 : 1;
+  }
+  else {
+    obj['[' + node.pos() + ']'] = '[' + node.pos() + ']' in obj ? obj['[' + node.pos() + ']'] + 1 : 1;
+  }
+  node.children().forEach(child => {
+    obj[node.pos() + child.pos()] = node.pos() + child.pos() in obj ? obj[node.pos() + child.pos()] + 1 : 1;
+    format2(child, obj);
+  });
+}
 
 function format(node, arr = []) {
   var currentJson = {}
--- test.py
+++ test.py
@@ -1,36 +1,74 @@
+print(__doc__)
 
 import sklearn
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
 from pymongo import MongoClient 
 from sklearn.feature_extraction import DictVectorizer
-from sklearn.feature_selection import VarianceThreshold
+from sklearn.feature_selection import SelectPercentile, chi2
+from sklearn import svm
 
+#Connect to MongoDB
 client = MongoClient(port=27017)
 db = client.jsAppTest
 coll = db.textData
 
-test = coll.find({})[0][u'arr']
-for parent in test:
-    for child in parent:
-        parent[child] = parent[child].encode('ascii', 'ignore')
-        parent[child.encode('ascii', 'ignore')] = parent.pop(child)
+#Query data and format
+query = coll.find({})
+X = []
+for row in query:
+    tempRow = {}
+    for child in row:        
+        if child != u'_id':
+            tempRow[child.encode('ascii', 'ignore')] = row[child]      
+    X.append(tempRow)
+
+#Fill in empty data
+df = pd.DataFrame(data=X)
+df.fillna(0, inplace=True)
+
+#Reformat
+for i in range(len(X)):
+    for row in df:
+        tempRow = {}
+        X[i][row] = df[row][i]
         
-vec = sklearn.feature_extraction.DictVectorizer()
-vec_vectorized = vec.fit_transform(test)
-X = vec_vectorized.toarray()
-y = [0] * X.shape[0]
-sel = VarianceThreshold(threshold=(.8 * (1 - .8)))
-X = sel.fit_transform(X, y)
-res = []
-for i in range(0, X.size):
-    if(X[i][0] > 0):
-        res.append(test[i])
-print res
+#Vectorize data and extract features
+vec = DictVectorizer()
+X = vec.fit_transform(X).toarray()
+y = np.arange(X.shape[0], dtype=np.int32)
 
+#Select features
+selector = SelectPercentile(chi2, percentile=10)
+selector.fit(X, y)
 
+#SVM classification
+clf = svm.SVC(kernel='linear')
+clf.fit(X, y)
+clf_selected = svm.SVC(kernel='linear')
+clf_selected.fit(selector.transform(X), y)
 
+#Calculate weights
+scores = -np.log10(selector.pvalues_)
+scores /= scores.max()
+svm_weights = (clf.coef_ ** 2).sum(axis=0)
+svm_weights /= svm_weights.max()
+svm_weights_selected = (clf_selected.coef_ ** 2).sum(axis=0)
+svm_weights_selected /= svm_weights_selected.max()
 
-
-
-
+#Plot data
+plt.figure(1)
+plt.clf()
+X_indices = np.arange(X.shape[-1])
+plt.bar(X_indices - .45, scores, width=.2, label=r'Univariate score ($-Log(p_{value})$)', color='darkorange', edgecolor='black')
+plt.bar(X_indices - .25, svm_weights, width=.2, label='SVM weight', color='navy', edgecolor='black')
+plt.bar(X_indices[selector.get_support()] - .05, svm_weights_selected, width=.2, label='SVM weights after selection', color='c', edgecolor='black')
+plt.title("Comparing feature selection")
+plt.xlabel('Feature number')
+plt.yticks(())
+plt.axis('tight')
+plt.legend(loc='upper right')
+plt.show()
 
 
