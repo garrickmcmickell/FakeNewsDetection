83cc85a7c18bb7a2697e65ee12949f1a2b6216b7
Garrick McMickell <garrickmcmickell@gmail.com>
2018-06-08 05:15:52 -0600

    Just saving.

---
+++ test2.py
@@ -0,0 +1,239 @@
+from __future__ import print_function
+print(__doc__)
+
+from time import time
+import numpy as np
+import matplotlib.pyplot as plt
+
+from sklearn import metrics
+from sklearn.cluster import KMeans
+from sklearn.datasets import load_digits
+from sklearn.decomposition import PCA
+from sklearn.preprocessing import scale
+
+
+
+from sklearn.datasets import fetch_20newsgroups
+from sklearn.decomposition import TruncatedSVD
+from sklearn.feature_extraction.text import TfidfVectorizer
+from sklearn.feature_extraction.text import HashingVectorizer
+from sklearn.feature_extraction.text import TfidfTransformer
+from sklearn.pipeline import make_pipeline
+from sklearn.preprocessing import Normalizer
+from sklearn import metrics
+
+from sklearn.cluster import KMeans, MiniBatchKMeans
+
+import logging
+from optparse import OptionParser
+import sys
+from time import time
+
+import numpy as np
+
+# Display progress logs on stdout
+logging.basicConfig(level=logging.INFO,
+                    format='%(asctime)s %(levelname)s %(message)s')
+
+# parse commandline arguments
+op = OptionParser()
+op.add_option("--lsa",
+              dest="n_components", type="int",
+              help="Preprocess documents with latent semantic analysis.")
+op.add_option("--no-minibatch",
+              action="store_false", dest="minibatch", default=True,
+              help="Use ordinary k-means algorithm (in batch mode).")
+op.add_option("--no-idf",
+              action="store_false", dest="use_idf", default=True,
+              help="Disable Inverse Document Frequency feature weighting.")
+op.add_option("--use-hashing",
+              action="store_true", default=False,
+              help="Use a hashing feature vectorizer")
+op.add_option("--n-features", type=int, default=10000,
+              help="Maximum number of features (dimensions)"
+                   " to extract from text.")
+op.add_option("--verbose",
+              action="store_true", dest="verbose", default=False,
+              help="Print progress reports inside k-means algorithm.")
+
+print(__doc__)
+op.print_help()
+
+
+def is_interactive():
+    return not hasattr(sys.modules['__main__'], '__file__')
+
+# work-around for Jupyter notebook and IPython console
+argv = [] if is_interactive() else sys.argv[1:]
+(opts, args) = op.parse_args(argv)
+if len(args) > 0:
+    op.error("this script takes no arguments.")
+    sys.exit(1)
+
+# #############################################################################
+
+from pymongo import MongoClient
+
+# Required function for tokenizing and preprocessing data since it is a list, not raw text
+def getChunks(sample):
+  for word in sample:
+    yield word
+
+#Names of classifiers saved to array for retreival, normalization, and idendification after normalization
+classifiers = ['straight', 'potential_bias', 'probable_bias', 'editorial', 'cherry', 'fake', 'satire']
+target_names = ['real', 'fake']
+
+#Connect to MongoDB
+client = MongoClient(port=27017)
+db = client.classifiedArticles
+
+#Query data by classifier
+queries = [db.articlePhraseChunked.find({'classifier': classifier}) for classifier in classifiers]
+
+#Format data, normalize classifier
+data = np.array([np.array([[item[key][i].encode('ascii', 'ignore') for i in range(len(item[key]))], 0 if classifier < 5 else 1])
+                for classifier in range(len(queries))
+                for item in queries[classifier]
+                for key in item if key == u'phraseChunks'])
+
+X_train = data[:, 0]
+y_train = data[:, 1].tolist()
+
+# #############################################################################
+
+print("%d documents" % data.shape[0])
+print("%d categories" % len(target_names))
+print()
+
+labels = y_train
+true_k = np.unique(labels).shape[0]
+
+print("Extracting features from the training dataset using a sparse vectorizer")
+t0 = time()
+if opts.use_hashing:
+    if opts.use_idf:
+        # Perform an IDF normalization on the output of HashingVectorizer
+        hasher = HashingVectorizer(n_features=opts.n_features,
+                                   stop_words='english', alternate_sign=False,
+                                   norm=None, binary=False, analyzer=getChunks)
+        vectorizer = make_pipeline(hasher, TfidfTransformer())
+    else:
+        vectorizer = HashingVectorizer(n_features=opts.n_features,
+                                       stop_words='english',
+                                       alternate_sign=False, norm='l2',
+                                       binary=False, analyzer=getChunks)
+else:
+    vectorizer = TfidfVectorizer(max_df=0.5, max_features=opts.n_features,
+                                 min_df=2, stop_words='english',
+                                 use_idf=opts.use_idf, analyzer=getChunks)
+X = vectorizer.fit_transform(X_train)
+
+print("done in %fs" % (time() - t0))
+print("n_samples: %d, n_features: %d" % X.shape)
+print()
+
+if opts.n_components:
+    print("Performing dimensionality reduction using LSA")
+    t0 = time()
+    # Vectorizer results are normalized, which makes KMeans behave as
+    # spherical k-means for better results. Since LSA/SVD results are
+    # not normalized, we have to redo the normalization.
+    svd = TruncatedSVD(opts.n_components)
+    normalizer = Normalizer(copy=False)
+    lsa = make_pipeline(svd, normalizer)
+
+    X = lsa.fit_transform(X)
+
+    print("done in %fs" % (time() - t0))
+
+    explained_variance = svd.explained_variance_ratio_.sum()
+    print("Explained variance of the SVD step: {}%".format(
+        int(explained_variance * 100)))
+
+    print()
+
+
+# #############################################################################
+# Do the actual clustering
+
+if opts.minibatch:
+    km = MiniBatchKMeans(n_clusters=true_k, init='k-means++', n_init=1,
+                         init_size=1000, batch_size=1000, verbose=opts.verbose)
+else:
+    km = KMeans(n_clusters=true_k, init='k-means++', max_iter=100, n_init=1,
+                verbose=opts.verbose)
+
+print("Clustering sparse data with %s" % km)
+t0 = time()
+km.fit(X)
+print('score: %0.3f' % km.score(X))
+print("done in %0.3fs" % (time() - t0))
+print()
+
+print("Homogeneity: %0.3f" % metrics.homogeneity_score(labels, km.labels_))
+print("Completeness: %0.3f" % metrics.completeness_score(labels, km.labels_))
+print("V-measure: %0.3f" % metrics.v_measure_score(labels, km.labels_))
+print("Adjusted Rand-Index: %.3f"
+      % metrics.adjusted_rand_score(labels, km.labels_))
+print("Silhouette Coefficient: %0.3f"
+      % metrics.silhouette_score(X, km.labels_, sample_size=1000))
+
+print()
+
+
+if not opts.use_hashing:
+    print("Top terms per cluster:")
+
+    if opts.n_components:
+        original_space_centroids = svd.inverse_transform(km.cluster_centers_)
+        order_centroids = original_space_centroids.argsort()[:, ::-1]
+    else:
+        order_centroids = km.cluster_centers_.argsort()[:, ::-1]
+
+    terms = vectorizer.get_feature_names()
+    for i in range(true_k):
+        print("Cluster %d:" % i, end='')
+        for ind in order_centroids[i, :10]:
+            print(' %s' % terms[ind], end='')
+        print()
+
+# #############################################################################
+# Visualize the results on PCA-reduced data
+
+#reduced_data = TruncatedSVD(n_components=2).fit_transform(X)
+kmeans = KMeans(init='k-means++', n_clusters=2, n_init=10)
+kmeans.fit(X)
+
+# Step size of the mesh. Decrease to increase the quality of the VQ.
+h = .02     # point in the mesh [x_min, x_max]x[y_min, y_max].
+
+# Plot the decision boundary. For that, we will assign a color to each
+x_min, x_max = X[:, 0].min() - 1, X[:, 0].max() + 1
+y_min, y_max = X[:, 1].min() - 1, X[:, 1].max() + 1
+xx, yy = np.meshgrid(np.arange(x_min, x_max, h), np.arange(y_min, y_max, h))
+
+# Obtain labels for each point in mesh. Use last trained model.
+Z = kmeans.predict(np.c_[xx.ravel(), yy.ravel()])
+
+# Put the result into a color plot
+Z = Z.reshape(xx.shape)
+plt.figure(1)
+plt.clf()
+plt.imshow(Z, interpolation='nearest',
+           extent=(xx.min(), xx.max(), yy.min(), yy.max()),
+           cmap=plt.cm.Paired,
+           aspect='auto', origin='lower')
+
+plt.plot(X[:, 0], X[:, 1], 'k.', markersize=2)
+# Plot the centroids as a white X
+centroids = kmeans.cluster_centers_
+plt.scatter(centroids[:, 0], centroids[:, 1],
+            marker='x', s=169, linewidths=3,
+            color='w', zorder=10)
+plt.title('K-means clustering on the digits dataset (PCA-reduced data)\n'
+          'Centroids are marked with white cross')
+plt.xlim(x_min, x_max)
+plt.ylim(y_min, y_max)
+plt.xticks(())
+plt.yticks(())
+plt.show()
---
+++ tf-idf.py
@@ -0,0 +1,158 @@
+from __future__ import print_function
+
+import logging
+import random
+import numpy as np
+import pandas as pd
+from optparse import OptionParser
+import sys
+
+from pymongo import MongoClient
+
+from sklearn.feature_extraction.text import TfidfVectorizer
+from sklearn.naive_bayes import BernoulliNB, MultinomialNB
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.linear_model import SGDClassifier
+from sklearn.linear_model import Perceptron
+from sklearn.utils.extmath import density
+from sklearn import metrics
+
+
+# Display progress logs on stdout
+logging.basicConfig(level=logging.INFO,
+                    format='%(asctime)s %(levelname)s %(message)s')
+
+# parse commandline arguments
+op = OptionParser()
+op.add_option('--random',
+              action='store', type='int', dest='num_samples',
+              help='Select n random test samples from each data set.')
+
+def is_interactive():
+    return not hasattr(sys.modules['__main__'], '__file__')
+
+# work-around for Jupyter notebook and IPython console
+argv = [] if is_interactive() else sys.argv[1:]
+(opts, args) = op.parse_args(argv)
+if len(args) > 0:
+    op.error("this script takes no arguments.")
+    sys.exit(1)
+
+print(__doc__)
+op.print_help()
+print()
+
+# Required function for tokenizing and preprocessing data since it is a list, not raw text.
+def getChunks(sample):
+  for word in sample:
+    yield word
+
+# #############################################################################
+# Query data
+
+# Names of classifiers saved to array for retreival, normalization, and idendification after normalization
+classifiers = ['straight', 'potential_bias', 'probable_bias', 'editorial', 'cherry', 'fake', 'satire']
+real_target_names = ['straight', 'potential_bias', 'probable_bias', 'editorial', 'cherry']
+fake_target_names = ['fake', 'satire']
+target_names = ['real', 'fake']
+
+# Connect to MongoDB
+client = MongoClient(port=27017)
+db = client.classifiedArticles
+
+# Query real data by classifier
+real_queries = [db.articlePhraseChunked.find({'classifier': classifier}) for classifier in real_target_names]
+
+# Query fake data by classifier
+fake_queries = [db.articlePhraseChunked.find({'classifier': classifier}) for classifier in fake_target_names]
+
+# Format real data, normalize classifier
+real_data = np.array([np.array([[item[key][i].encode('ascii', 'ignore') for i in range(len(item[key]))], 0])
+                                                                        for classifier in range(len(real_queries))
+                                                                        for item in real_queries[classifier]
+                                                                        for key in item if key == u'phraseChunks'])
+
+# Format fake data, normalize classifier
+fake_data = np.array([np.array([[item[key][i].encode('ascii', 'ignore') for i in range(len(item[key]))], 1])
+                                                                        for classifier in range(len(fake_queries))
+                                                                        for item in fake_queries[classifier]
+                                                                        for key in item if key == u'phraseChunks'])
+
+# #############################################################################
+# Function for getting sample data
+
+def getData():
+  # Get 5 random samples from each data set
+  test_indicies_real = random.sample(range(0, real_data.shape[0]), 5)
+  test_real = np.array([real_data[index] for index in test_indicies_real])
+  test_indicies_fake = random.sample(range(0, fake_data.shape[0]), 5)
+  test_fake = np.array([fake_data[index] for index in test_indicies_fake])
+
+  # Set train samples as data retrieved without test samples
+  train_real = np.delete(real_data, test_indicies_real, 0)
+  train_fake = np.delete(fake_data, test_indicies_fake, 0)
+
+  # Made train and test variables using train and test samples
+  X_train = np.concatenate((train_real[:, 0], train_fake[:, 0]), axis=0)
+  y_train = np.concatenate((train_real[:, 1], train_fake[:, 1]), axis=0).tolist()
+  X_test = np.concatenate((test_real[:, 0], test_fake[:, 0]), axis=0)
+  y_test = np.concatenate((test_real[:, 1], test_fake[:, 1]), axis=0).tolist()
+
+  # Return sample data
+  return X_train, y_train, X_test, y_test
+
+# #############################################################################
+# Funtion to vectorize data
+
+def vectorize(X_train, X_test):
+  # Create vectorizer
+  vectorizer = TfidfVectorizer(analyzer=getChunks, sublinear_tf=True, max_df=0.2, stop_words='english')
+
+  # Fit/tranform train data and transform test data
+  X_train = vectorizer.fit_transform(X_train)
+  X_test = vectorizer.transform(X_test)
+
+  # Return vectorized data
+  return X_train, X_test
+
+# #############################################################################
+# Function to enchmark classifiers
+
+def benchmark(clf, X_train, y_train, X_test, y_test):
+  clf.fit(X_train, y_train)
+  pred = clf.predict(X_test)
+
+  score = metrics.accuracy_score(y_test, pred)
+
+  return score
+
+# #############################################################################
+
+results = []
+
+for i in range(100):
+  # Get a new set of data
+  X_train, y_train, X_test, y_test = getData()
+
+  # Vectorize the new data
+  X_train, X_test = vectorize(X_train, X_test)
+
+  # Run data through classifiers
+  predictions = np.array([benchmark(clf, X_train, y_train, X_test, y_test) for clf in (MultinomialNB(alpha=.01),
+                                                                                       BernoulliNB(alpha=0.0128125),
+                                                                                       SGDClassifier(alpha=.0001, max_iter=50, penalty='l1'),
+                                                                                       Perceptron(max_iter=50),
+                                                                                       RandomForestClassifier(n_estimators=100))])
+  
+  results.append(predictions)
+
+  print('Test %i of 100 Complete.' % (i + 1))
+
+results = np.array(results)
+results = pd.DataFrame({'MultiNB':  results[:, 0],
+                        'BernNB':   results[:, 1],
+                        'SGD':      results[:, 2],
+                        'Percept':  results[:, 3],
+                        'RandFrst': results[:, 4]})
+
+results.to_excel('classifierResults4.xlsx', sheet_name='sheet1', index=False)
